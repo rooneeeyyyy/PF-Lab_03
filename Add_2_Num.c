7de1b24f23a21ed0cb37ca30900aa87a6f3ecb93
Ruhaan_Hussain <ruhaanh2@gmail.com>
2026-09-11 02:26:07 +0500

    Added a new C program

---
+++ Add_2_Num.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int num1, num2, sum;
+    printf("Enter the first number: \n");
+    scanf("%d",&num1);
+    printf("Enter the second number: \n");
+    scanf("%d",&num2);
+    sum = num1 + num2;
+    printf("The sum of two number is: %d \n",sum);
+}
