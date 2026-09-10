682214b1c9cef088556c3e7b3b361e5083041613
Ruhaan_Hussain <ruhaanh2@gmail.com>
2026-09-11 02:46:24 +0500

    New program added

---
+++ Output_Practice.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[] = "Ruhaan";
+    int age = 18;
+    char city[] = "Karachi";
+
+    printf("Name:\t\"%s\"\n", name);
+    printf("Age:\t %d\n", age);
+    printf("City:\t\"%s\"\n", city);
+}
